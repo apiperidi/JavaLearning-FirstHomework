5c70a2c98c6ed7f57867e6ffdb42b52c32c1ea7d
apiperidi <apiperidi@nbg.gr>
2021-03-22 10:30:33 +0200

    add .ideal/ ang *.iml

--- src/main/java/com/example/database/DBdemo.java
+++ src/main/java/com/example/database/DBdemo.java
@@ -1,25 +1,28 @@
 package com.example.database;
 
+import com.thedeanda.lorem.Lorem;
+import com.thedeanda.lorem.LoremIpsum;
 import org.h2.tools.Server;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
+import java.util.Arrays;
 import java.util.Properties;
 
 import static java.lang.System.exit;
 
 public class DBdemo {
 
-    private static final String DB_URL = "jdbc:h2:mem:sample"; //memory database| name: sample
+    private static final String DB_URL = "jdbc:h2:mem:sample"; //memory database στο heap| name: sample
     private static final String DB_USERNAME = "sa";
     private static final String DB_PASSWORD = "";
     private final Properties sqlCommands = new Properties(); //η διεύθυνση στην μνήμη που έχει πάρει το object properties
-
+private final Lorem generator = LoremIpsum.getInstance();
     private Server h2Server;
 
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args)  {
         DBdemo demo = new DBdemo();
         // Load SQL commands
         demo.loadSqlCommands();
@@ -28,21 +31,48 @@ public class DBdemo {
         demo.startH2Server();
         // Register JDBC driver and retrieve a connection
         Connection connection = demo.registerDatabaseDriver();
+
+       //θα θέλει commit
+        enableTrancactionMode(connection);
+
+
         // Create table
         demo.createTable(connection);
         // Insert Data
-demo.InsertTable(connection);
-demo.SelectTable(connection);
-demo.UpdateTable(connection);
+        demo.InsertTable(connection);
+        demo.InsertTableBatch(connection);
+        demo.InsertTableBatchGenerator(connection);
+        try {
+            connection.commit();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        //
+        demo.SelectTable(connection);
+        demo.UpdateTable(connection);
         demo.SelectTable(connection);
         // SQL Commands
-
+        demo.DeleteTable(connection);
         //Introduce artificial delay
-        Thread.sleep(3000);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         // Stop H2 database server
         demo.stopH2Server();
     }
+
+    private static void enableTrancactionMode(Connection connection) {
+        try {
+            connection.setAutoCommit(false);  //Εάν βάλω false θα πρέπει να κάνω commit ή rollback
+        } catch (SQLException throwables) {
+
+            throwables.printStackTrace();
+        }
+    }
+
 
     private void loadSqlCommands() {
         try (InputStream inputStream = DBdemo.class.getClassLoader().getResourceAsStream("sql.properties")) {
@@ -116,49 +146,107 @@ demo.UpdateTable(connection);
     private void InsertTable(Connection connection) {
         try (Statement statement = connection.createStatement()) {
             int resultRows=0;
-            resultRows= statement.executeUpdate(sqlCommands.getProperty("insert.table.REGISTRATION"));
+            resultRows  = statement.executeUpdate(sqlCommands.getProperty("insert.table.REGISTRATION"));
             resultRows += statement.executeUpdate(sqlCommands.getProperty("insert.table.REGISTRATION2"));
+            resultRows += statement.executeUpdate(sqlCommands.getProperty("insert.table.REGISTRATION3"));
+            resultRows += statement.executeUpdate(sqlCommands.getProperty("insert.table.REGISTRATION4"));
+            resultRows += statement.executeUpdate(sqlCommands.getProperty("insert.table.REGISTRATION5"));
+
             System.out.println("Statement inserted " + resultRows);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+            System.err.println("Error occurred while inserting data");
         }
-        finally {
 
+    }
+
+    private void InsertTableBatch(Connection connection) {
+        try ( PreparedStatement prStatement = connection.prepareStatement(sqlCommands.getProperty("insertBatch.table.REGISTRATION"))) {
+            prStatement.setLong(1,6);
+            prStatement.setString(2,"Manos");
+            prStatement.setString(3,"Rallis");
+            prStatement.setInt(4,10);
+            prStatement.addBatch();
+            prStatement.clearParameters();
+
+            prStatement.setLong(1,7);
+            prStatement.setString(2,"Anastasios");
+            prStatement.setString(3,"Rallis");
+            prStatement.setInt(4,18);
+            prStatement.addBatch();
+
+            int[] resultRows =prStatement.executeBatch();
+            System.out.println("Statement inserted " + Arrays.stream(resultRows).sum());
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            System.err.println("Error occurred while inserting data");
+        }
+
+    }
+
+    private void generateData (PreparedStatement prStatement, int HowMany) throws SQLException {
+        for (int i=0; i<HowMany; i++) {
+            prStatement.clearParameters();
+            prStatement.setLong(1, 8 + i);
+            prStatement.setString(2, generator.getFirstName());
+            prStatement.setString(3, generator.getLastName());
+            prStatement.setInt(4, 50);
+            prStatement.addBatch();
         }
     }
 
+    private void InsertTableBatchGenerator(Connection connection) {
+        try ( PreparedStatement prStatement = connection.prepareStatement(sqlCommands.getProperty("insertBatch.table.REGISTRATION"))) {
+
+            generateData(prStatement,10);
+            int[] resultRows =prStatement.executeBatch();
+            System.out.println("Statement inserted " + Arrays.stream(resultRows).sum());
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            System.err.println("Error occurred while inserting data");
+        }
+
+    }
+
+
 
     private void SelectTable(Connection connection) {
-        try (Statement statement = connection.createStatement()) {
-            ResultSet rs= statement.executeQuery(sqlCommands.getProperty("select.table.REGISTRATION"));
-         while (rs.next())
+        try (Statement statement = connection.createStatement();
+             ResultSet rs= statement.executeQuery(sqlCommands.getProperty("select.table.REGISTRATION")) )
+        {
+             while (rs.next())
          {
-             System.out.printf("ID: %s. Name : %s, Age: %s ",rs.getInt("ID"),rs.getString("LASTNAME"),rs.getInt("AGE") );
+             System.out.printf("ID: %s. Name: %s. Age: %s ",rs.getLong("ID"),rs.getString("LASTNAME"),rs.getInt("AGE") );
              System.out.println();
          }
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+            System.err.println("Error occurred while selecting data");
         }
-        finally {
 
-        }
     }
 
     private void UpdateTable(Connection connection) {
-        String query ="Update REGISTRATION set Age =? where FIRSTNAME=?";
+        String query ="Update REGISTRATION set Age =AGE+5 where FIRSTNAME=?";
         try{
             PreparedStatement prStatement = connection.prepareStatement(query);
-            prStatement.setInt(1,8);
-            prStatement.setString(2,"Xara");
+           // prStatement.setInt(1,8);
+            prStatement.setString(1,"Xara");
           int resultRowsUpdated=  prStatement.executeUpdate();
             System.out.println("Statement inserted " + resultRowsUpdated);
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+            System.err.println("Error occurred while updating data");
         }
-        finally {
 
-        }
     }
+
+    private void DeleteTable(Connection connection) {
+    }
+
+
 }
